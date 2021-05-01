5cfe94f9f60e609987916915f8e40b671425cb1c
Yunus Emre Atik <43673416+yunusatik@users.noreply.github.com>
2021-05-01 14:29:44 +0300

    Add files via upload

---
+++ LudoServer/src/ludoserver/RunServer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ludoserver;
+
+/**
+ *
+ * @author Yunus Emre Atik (yunusemre.atik@stu.fsm.edu.tr)
+ */
+public class RunServer {
+
+    public static void main(String[] args) {
+        Server.Start(2000);
+    }
+}
